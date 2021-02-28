9e1d688a2e5a2cb02db38d68c0679aa8bc29b348
Vadym <vsamkovich@ukr.net>
2021-02-28 17:48:18 +0100

    created firebase db

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ShopPage from './pages/shop';
 import SignInAndSignUp from './pages/sign-in-and-sign-up';
 import Header from './components/header';
 import {Route, Switch} from 'react-router-dom';
-import {auth} from './firebase/firebase';
+import {auth, createUserProfileDocument} from './firebase/firebase';
 import './App.css';
 
 
@@ -17,8 +17,21 @@ export default class App extends Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({currentUser: user});
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					})	
+				})
+			} else {
+				this.setState({currentUser: userAuth})
+			}
 		})
 	}
 
